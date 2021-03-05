f8c2c1aa39e2f87193ae3ac613880fdd23570fd7
Mari-8 <ryanaltimari8@gmail.com>
2021-03-05 17:11:17 -0600

    commit tree 921825c45cf0b2c694b1fd0e4cf69b6918995378

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+
+
+function submitData(name, email) {
+
+    let userData = {
+        name: `${name}`,
+        email: `${email}`
+    }
+
+    let userConfig = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json", 
+            "accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    }
+
+    let main = document.body
+
+    return fetch("http://localhost:3000/users", userConfig)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object){
+            let newId = document.createElement('h1')
+            newId.innerHTML = object.id
+            main.appendChild(newId)
+        })
+        .catch(function(error) {
+            alert("Not Working");
+            let errorMessage = document.createElement('p')
+            errorMessage.innerHTML = `${error.message}`
+            main.appendChild(errorMessage)
+          }); 
+}
